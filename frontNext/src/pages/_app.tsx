d468d301e4953bd4a9b4f12e11f438f8123f88d6
Дмитриев Андрей <62550256+agd01991@users.noreply.github.com>
2024-11-26 17:52:23 +0300

    Update _app.tsx

--- frontNext/src/pages/_app.tsx
+++ frontNext/src/pages/_app.tsx
@@ -1,21 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-    (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-    m[i].l=1*new Date();
-    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(93081907, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
-  }, []);
-  
+export default function App({ Component, pageProps }: AppProps) {  
   return (
     <Component {...pageProps} />)
 }
