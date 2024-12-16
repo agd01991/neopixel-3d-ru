a0cab6b4812d399b150291a55c69828831f020e5
Дмитриев Андрей <62550256+agd01991@users.noreply.github.com>
2024-12-16 21:13:07 +0300

    Update _document.tsx

--- frontNext/src/pages/_document.tsx
+++ frontNext/src/pages/_document.tsx
@@ -1,27 +1,34 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html lang="en">
-          <Head>
-      <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(93081907, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/93081907" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-    </Head>
+          ym(93081907, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+          });
+        `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/93081907" style={{position:'absolute', left:'-9999px'}} alt="" />
+        </div>
+      </noscript>
       <body>
         <Main />
         <NextScript />
